433df418d43d209b6bfa134f51eaf3e2e46013e1
nanda-putri <putrifebriantono@gmail.com>
2022-03-11 10:59:28 +0700

    menambahkan folder tugas2paw dan file hello.php

---
+++ tugas2paw/hello.php
@@ -0,0 +1,4 @@
+<?php // pembuka teks php
+echo "Nama : Nanda Putri Febri Antono"."<br>" ; //untuk menampilkan nama dan br untuk memisah dengan baris selanjutnya
+echo "NIM : 200411100036"; //untuk menampilkan NIM 
+?>
