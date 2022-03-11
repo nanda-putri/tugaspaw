e709b6a3b303313c95512ffbee8e68070229b32d
nanda-putri <putrifebriantono@gmail.com>
2022-03-11 11:07:05 +0700

    menambahkan file variable.php dalam folder tugas2paw

---
+++ tugas2paw/variable.php
@@ -0,0 +1,10 @@
+<?php  //pembuka tag php
+
+$a=8; //membuat variabel a bertipe integer dengan nilai 8 
+$b=3; //membuat variabel b bertipe integer dengan nilai 3 
+$c=9; //membuat variabel c bertipe integer dengan nilai 89
+
+echo "Hasil penjumlahan dari $a + $b = ".$a+$b."<br>"; //melakukan penjumlahan dari variabel a dan b
+echo "Hasil pengurangan dari $c - $b = ".$c-$b."<br>"; //melakukan pengurangan dari variabel c dan b
+echo "Hasil perkalian dari $c x $a = ".$c*$a; //melakukan perkalian dari variabel c dan a
+?>
