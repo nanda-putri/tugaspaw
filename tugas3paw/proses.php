9ff2d69f0bc838cf4084cdbeb5db6c54e248678a
nanda-putri <putrifebriantono@gmail.com>
2022-03-31 15:41:34 +0700

    menambah dan mengubah syntax pada file proses.php

--- tugas3paw/proses.php
+++ tugas3paw/proses.php
@@ -9,7 +9,8 @@ if (isset($_POST['submit'])){
 	$query="INSERT INTO tbl_036(NIM,Nama,Asal,Telepon) VALUES ('$NIM','$Nama','$Asal','$Telepon')";
 	$result=mysqli_query($connect,$query);
 	if ($result) {
-		header('location:hasil.php');
+		echo "<script> alert('data berhasil ditambahkan');window.location='hasil.php'</script>";
+		// header('location:hasil.php');
 	} else{
 		echo "gagal menambah data";
 	}
